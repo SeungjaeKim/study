2a19fe62860e425231ecccf4744d6cc5bb2eec71
yong <yong@AC920996>
2018-07-19 13:20:48 +0900

    서버 기동시 배너 추가

--- src/main/java/com/study/StudyApplication.java
+++ src/main/java/com/study/StudyApplication.java
@@ -1,12 +1,19 @@
 package com.study;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude=DataSourceAutoConfiguration.class)
 public class StudyApplication {
 
 	public static void main(String[] args) {
+		/**
+		 * - 서버 기동시 콘솔에 배너를 출력한다.
+		 *     배너기본경로 : /study/src/main/resources/banner.txt
+		 */
 		SpringApplication.run(StudyApplication.class, args);
 	}
 }
