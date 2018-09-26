f3432ff2fc0b0fe5e810addff16b8a84752db65b
choi <choi@Lenovo-PC>
2018-09-26 16:16:09 +0900

    -

--- src/main/java/com/study/domain/Board.java
+++ src/main/java/com/study/domain/Board.java
@@ -1,13 +1,19 @@
 package com.study.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
-import java.sql.Timestamp;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
